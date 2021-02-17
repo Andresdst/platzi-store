76e655173582217309cda1bfc02f4a541f3037af
Andresdst <andres-network@hotmail.com>
2021-02-17 10:18:43 -0400

    transformando a total en un observable, utilizando el pipe async desde el template angular se encarga de subscribirse y desubscribirse cuando no se este utilizando

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import {map} from 'rxjs/operators'
 import { CartService } from '../../../core/services/cart.service'
 
@@ -10,14 +11,11 @@ import { CartService } from '../../../core/services/cart.service'
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0
+  total$: Observable<number>
 
   constructor(private cartService:CartService) {
-    this.cartService.cart$
-    .pipe(map(products=>products.length))
-    .subscribe(total=>{
-      this.total = total
-    })
+    this.total$ = this.cartService.cart$
+      .pipe(map(products=>products.length))
   }
 
   ngOnInit(): void {
